c2b4a7e68a437b834a9918573bbee81db17c0e73
qais25 <qaisabdullahbafayadh@gmail.com>
2024-10-27 16:04:52 +0300

    Update q_pop_icon.dart

--- lib/core/shared_widgets/q_pop_icon.dart
+++ lib/core/shared_widgets/q_pop_icon.dart
@@ -1,17 +1,17 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class PopIcon extends StatefulWidget {
-  const PopIcon({super.key});
+// class PopIcon extends StatefulWidget {
+//   const PopIcon({super.key});
 
-  @override
-  State<PopIcon> createState() => _PopIconState();
-}
+//   @override
+//   State<PopIcon> createState() => _PopIconState();
+// }
 
-class _PopIconState extends State<PopIcon> {
-  @override
-  Widget build(BuildContext context) {
-    return IconButton( onPressed: () {
-            Navigator.of(context).pop();
-          }, icon: const Icon(Icons.arrow_back,size: 30,color: Colors.black,),);
-  }
-}
+// class _PopIconState extends State<PopIcon> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return IconButton( onPressed: () {
+//             Navigator.of(context).pop();
+//           }, icon: const Icon(Icons.arrow_back,size: 30,color: Colors.black,),);
+//   }
+// }
