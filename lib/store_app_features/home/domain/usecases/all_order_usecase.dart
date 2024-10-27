03f8b4e27c0b0e3a42b00e0140f703e3dcbd157b
qais25 <qaisabdullahbafayadh@gmail.com>
2024-10-27 18:19:49 +0300

    Update all_order_usecase.dart

--- lib/store_app_features/home/domain/usecases/all_order_usecase.dart
+++ lib/store_app_features/home/domain/usecases/all_order_usecase.dart
@@ -1,30 +1,32 @@
-// import 'package:dartz/dartz.dart';
-// import 'package:sindbad_management_app/core/errors/failure.dart';
-// import 'package:sindbad_management_app/core/use_cases/param_use_case.dart';
-// import 'package:sindbad_management_app/store_app_features/home/domain/entities/all_order.dart';
-// import 'package:sindbad_management_app/store_app_features/home/domain/repos/all_order_repo.dart';
+import 'package:dartz/dartz.dart';
+import 'package:sindbad_management_app/core/errors/failure.dart';
+import 'package:sindbad_management_app/core/use_cases/param_use_case.dart';
+import 'package:sindbad_management_app/store_app_features/home/domain/repos/all_order_repo.dart';
 
-// class AllOrderUsecase
-//     extends UseCaseWithParam<List<AllOrderEntity>, AllOraderParam> {
-//   final AllOrderRepo allOrderRepo;
+import '../entities/all_order_entity.dart';
 
-//   AllOrderUsecase(this.allOrderRepo);
+class AllOrderUsecase
+    extends UseCaseWithParam<List<AllOrderEntity>, AllOraderParam> {
+  final AllOrderRepo allOrderRepo;
 
-//   @override
-//   Future<Either<Failure, List<AllOrderEntity>>> execute(
-//       AllOraderParam params)async {
-//         return await allOrderRepo.fetchAllOrder(pageNumber: params.pageNumber,
-//         pageSize: params.pageSize,
-//         storeId: params.storeId,
-//         srearchKeyword: params.srearchKeyword)
-//       }
-// }
+  AllOrderUsecase(this.allOrderRepo);
 
-// class AllOraderParam {
-//   final String storeId;
-//   final int srearchKeyword;
-//   final int pageNumber;
-//   final int pageSize;
-//   AllOraderParam(
-//       this.pageNumber, this.pageSize, this.storeId, this.srearchKeyword);
-// }
+  @override
+  Future<Either<Failure, List<AllOrderEntity>>> execute(
+      AllOraderParam params) async {
+    return await allOrderRepo.fetchAllOrder(
+        pageNumber: params.pageNumber,
+        pageSize: params.pageSize,
+        storeId: params.storeId,
+        srearchKeyword: params.srearchKeyword);
+  }
+}
+
+class AllOraderParam {
+  final String storeId;
+  final int srearchKeyword;
+  final int pageNumber;
+  final int pageSize;
+  AllOraderParam(
+      this.pageNumber, this.pageSize, this.storeId, this.srearchKeyword);
+}
