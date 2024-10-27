2b9e7439921b68abb6d233636ea35d75d51b0f72
qais25 <qaisabdullahbafayadh@gmail.com>
2024-10-27 16:01:38 +0300

    Update store_list_view_offer.dart

--- lib/store_app_features/offers/ui/widget/store_list_view_offer.dart
+++ lib/store_app_features/offers/ui/widget/store_list_view_offer.dart
@@ -1,26 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:sindbad_management_app/core/styles/Colors.dart';
-import 'package:sindbad_management_app/core/styles/text_style.dart';
-import 'package:sindbad_management_app/core/widgets/custom_primary_button_widget.dart';
+import 'package:sindbad_management_app/store_app_features/offers/ui/widget/offer_overview_widget.dart';
 
 class StoreListViewOffer extends StatelessWidget {
   const StoreListViewOffer(
       {super.key,
-      required this.offerStart,
-      required this.offerFinish,
-      required this.categorys,
-      required this.offerType,
-      required this.category,
-      required this.discountRate,
-      required this.bouns});
-  final String offerStart;
-  final String offerFinish;
-  final String offerType;
-  final String categorys;
-  final String category;
-  final String discountRate;
-  final String bouns;
+      this.offerStart,
+      this.offerFinish,
+      this.excelFile,
+      this.offerType,
+      this.category,
+      this.discountRate,
+      this.bouns});
+  final String? offerStart;
+  final String? offerFinish;
+  final String? offerType;
+  final String? excelFile;
+  final String? category;
+  final String? discountRate;
+  final String? bouns;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -29,133 +27,15 @@ class StoreListViewOffer extends StatelessWidget {
       child: ListView.builder(
           itemCount: 10,
           itemBuilder: (context, i) {
-            return Container(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 10.h,
-                ),
-                margin: EdgeInsets.symmetric(vertical: 4.h, horizontal: 4.w),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5.r),
-                    color: AppColors.primaryColor,
-                    boxShadow: const [
-                      BoxShadow(
-                          color: Colors.grey,
-                          offset: Offset(-1, 1),
-                          blurRadius: 1)
-                    ]),
-                width: 370.w,
-                height: 500.h,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      height: 100.h,
-                      child: Column(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.symmetric(
-                                vertical: 5.h, horizontal: 5.w),
-                            child: Row(
-                              children: [
-                                Text(
-                                  "بداية العرض:  ",
-                                  style: KTextStyle.textStyle12
-                                      .copyWith(color: AppColors.greyHint),
-                                ),
-                                Text(
-                                  offerStart,
-                                  style: KTextStyle.secondaryTitle,
-                                ),
-                              ],
-                            ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.symmetric(
-                                vertical: 5.h, horizontal: 5.w),
-                            child: Row(
-                              children: [
-                                Text("نهاية العرض:  ",
-                                    style: KTextStyle.textStyle12
-                                        .copyWith(color: AppColors.greyHint)),
-                                Text(offerFinish,
-                                    style: KTextStyle.secondaryTitle),
-                              ],
-                            ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.symmetric(
-                                vertical: 5.h, horizontal: 5.w),
-                            child: Row(
-                              children: [
-                                Text("نوع العرض:  ",
-                                    style: KTextStyle.textStyle12
-                                        .copyWith(color: AppColors.greyHint)),
-                                Text(offerType,
-                                    style: KTextStyle.secondaryTitle),
-                              ],
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(" الاصناف: ",
-                                  style: KTextStyle.textStyle12
-                                      .copyWith(color: AppColors.greyHint)),
-                              Text(categorys, style: KTextStyle.secondaryTitle),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      height: 85.h,
-                      margin: EdgeInsets.symmetric(vertical: 10.h),
-                      decoration:
-                          const BoxDecoration(color: AppColors.scaffColor),
-                      alignment: Alignment.topRight,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: [
-                              Text("الصنف:  ",
-                                  style: KTextStyle.secondaryTitle
-                                      .copyWith(fontWeight: FontWeight.w500)),
-                              Text(category, style: KTextStyle.secondaryTitle)
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Text("نسبة الخصم:  ",
-                                  style: KTextStyle.secondaryTitle
-                                      .copyWith(fontWeight: FontWeight.w500)),
-                              Text(discountRate)
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Text("البونص:  ",
-                                  style: KTextStyle.secondaryTitle
-                                      .copyWith(fontWeight: FontWeight.w500)),
-                              Text(bouns, style: KTextStyle.secondaryTitle)
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 250.h,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        KCustomPrimaryButtonWidget(
-                            buttonName: "تنفيذ", onPressed: () {})
-                      ],
-                    )
-                  ],
-                ));
+            return const OfferOverviewWidget(
+              offerStart: '2024/10/2',
+              offerFinish: '2024/10/4',
+              offerType: 'خصم/بونص',
+              excelFile: 'ملف اكسل',
+              category: '',
+              discountRate: '',
+              bouns: '',
+            );
           }),
     );
   }
