be06431b9a0fca29d1b5089be4e779c145a7ec63
qais25 <qaisabdullahbafayadh@gmail.com>
2024-10-27 16:03:03 +0300

    Update store_home_page_order_widget.dart

--- lib/store_app_features/home/ui/widget/store_home_page_order_widget.dart
+++ lib/store_app_features/home/ui/widget/store_home_page_order_widget.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:sindbad_management_app/core/styles/text_style.dart';
 import 'package:sindbad_management_app/core/widgets/custom_search_widget.dart';
 import 'package:sindbad_management_app/core/widgets/custom_tap_bar_widget.dart';
-import 'package:sindbad_management_app/store_app_features/home/ui/widget/store_home_page_list_view_widget.dart';
+import 'package:sindbad_management_app/store_app_features/home/ui/widget/store_home_page_list_view_before.dart';
+import 'package:sindbad_management_app/store_app_features/home/ui/widget/store_home_page_list_view_cancel.dart';
+import 'package:sindbad_management_app/store_app_features/home/ui/widget/store_home_page_list_view_new.dart';
+import 'package:sindbad_management_app/store_app_features/home/ui/widget/store_home_page_list_view_quick.dart';
 
 class StoreHomePageOrderWidget extends StatelessWidget {
   const StoreHomePageOrderWidget({super.key});
@@ -38,29 +41,13 @@ class StoreHomePageOrderWidget extends StatelessWidget {
             ),
           ], tabViews: const [
             //New
-            StoreHomePageListViewWidget(
-              bondNum: '654654',
-              date: '2024/10/2',
-              itemNum: '1',
-            ),
+            StoreHomePageListViewNew(),
             //Quaick
-            StoreHomePageListViewWidget(
-              bondNum: '654654',
-              date: '2024/10/2',
-              itemNum: '2',
-            ),
+            StoreHomePageListViewQuick(),
             //Before
-            StoreHomePageListViewWidget(
-              bondNum: '654654',
-              date: '2024/10/2',
-              itemNum: '3',
-            ),
+            StoreHomePageListViewBefore(),
             //Cancled
-            StoreHomePageListViewWidget(
-              bondNum: '654654',
-              date: '2024/10/2',
-              itemNum: '4',
-            ),
+            StoreHomePageListViewCancel(),
           ], length: 4),
         ],
       ),
