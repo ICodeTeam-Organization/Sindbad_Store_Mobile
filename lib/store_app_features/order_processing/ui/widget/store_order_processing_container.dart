219d8289f941fa87f8ec24f081daa07f4a2f6c45
qais25 <qaisabdullahbafayadh@gmail.com>
2024-10-27 16:00:22 +0300

    Update store_order_processing_container.dart

--- lib/store_app_features/order_processing/ui/widget/store_order_processing_container.dart
+++ lib/store_app_features/order_processing/ui/widget/store_order_processing_container.dart
@@ -1,57 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:sindbad_management_app/core/styles/Colors.dart';
-import 'package:sindbad_management_app/core/styles/text_style.dart';
+import 'package:sindbad_management_app/store_app_features/order_processing/ui/widget/store_order_processing_container_body.dart';
+
 class StoreOrderProcessingContainer extends StatelessWidget {
-  const StoreOrderProcessingContainer({super.key, required this.numberOrder, required this.date, required this.numberItem,});
-  final String numberOrder;
-  final String date;
-  final String numberItem;
+  const StoreOrderProcessingContainer({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 8.h, horizontal: 8.w),
-      decoration: BoxDecoration(
-        color: const Color(0xff454545),
-        borderRadius: BorderRadius.circular(10.r)
-      ),
-      width: 344.w,
-      height: 80.h,
-
-      child:   Padding(
-        padding: EdgeInsets.symmetric(vertical: 8.h, horizontal: 8.w),
-        child:  Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("رقم الطلب:  ", style: KTextStyle.secondaryTitle.copyWith(color: AppColors.white)),
-                Text(numberOrder, style: KTextStyle.secondaryTitle.copyWith(fontWeight: FontWeight.w400,color: AppColors.white),)
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Row(
-                  children: [
-                    Text( "التاريخ : ", style: KTextStyle.secondaryTitle.copyWith(color: AppColors.white)),
-                    Text(date, style: KTextStyle.secondaryTitle.copyWith(fontWeight: FontWeight.w400,color: AppColors.white)),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text("عدد الاصناف : ", style: KTextStyle.secondaryTitle.copyWith(color: AppColors.white)),
-                    Text(numberItem, style: KTextStyle.secondaryTitle.copyWith(fontWeight: FontWeight.w400,color: AppColors.white))
-                  ],
-                )
-              ],
-            )
-          ],
-        ),
-      ),
+    return const StoreOrderProcessingContainerBody(
+      numberOrder: '2622',
+      date: '2024/10/2',
+      numberItem: '5',
     );
   }
 }
