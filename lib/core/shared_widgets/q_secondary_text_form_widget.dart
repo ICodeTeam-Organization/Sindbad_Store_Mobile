0e10f3c67e3aab8c7e4e062c6a559055f5f78c47
qais25 <qaisabdullahbafayadh@gmail.com>
2024-10-27 16:04:37 +0300

    Update q_secondary_text_form_widget.dart

--- lib/core/shared_widgets/q_secondary_text_form_widget.dart
+++ lib/core/shared_widgets/q_secondary_text_form_widget.dart
@@ -1,38 +1,38 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class SecondaryTextFormWidget extends StatelessWidget {
-  const SecondaryTextFormWidget({super.key, required this.texthint});
-  final String texthint ;
+// class SecondaryTextFormWidget extends StatelessWidget {
+//   const SecondaryTextFormWidget({super.key, required this.texthint});
+//   final String texthint ;
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      height: 30.h,
-      width: 150.w,
-      margin: const EdgeInsets.all(5),
-      child: TextFormField(
-              decoration:  InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(100.r),
-                  borderSide:  BorderSide(
-                    color: Colors.black,
-                    width: 1.w
-                  )
-                ),
-                focusedBorder:  OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(100.r),
-                  borderSide:  BorderSide(
-                    color: Colors.black,
-                    width: 1.w
-                  )
-                ),
-                //prefixIcon: const Icon(Icons.search),
-                hintText: texthint,
-                hintStyle: TextStyle( color: const Color.fromARGB(255, 80, 80, 80), fontSize: 16.sp,)
-              ),
-            ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       alignment: Alignment.center,
+//       height: 30.h,
+//       width: 150.w,
+//       margin: const EdgeInsets.all(5),
+//       child: TextFormField(
+//               decoration:  InputDecoration(
+//                 enabledBorder: OutlineInputBorder(
+//                   borderRadius: BorderRadius.circular(100.r),
+//                   borderSide:  BorderSide(
+//                     color: Colors.black,
+//                     width: 1.w
+//                   )
+//                 ),
+//                 focusedBorder:  OutlineInputBorder(
+//                   borderRadius: BorderRadius.circular(100.r),
+//                   borderSide:  BorderSide(
+//                     color: Colors.black,
+//                     width: 1.w
+//                   )
+//                 ),
+//                 //prefixIcon: const Icon(Icons.search),
+//                 hintText: texthint,
+//                 hintStyle: TextStyle( color: const Color.fromARGB(255, 80, 80, 80), fontSize: 16.sp,)
+//               ),
+//             ),
+//     );
+//   }
+// }
