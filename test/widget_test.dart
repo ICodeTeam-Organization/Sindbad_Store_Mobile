2e438ded62156ef406cb59ad9f910d48be477e91
qais25 <qaisabdullahbafayadh@gmail.com>
2024-10-27 15:49:07 +0300

    edit :  name project in test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:sindbad_store_app/main.dart';
+import 'package:sindbad_management_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const SindbadManagementApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
