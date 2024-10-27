e70af1cac8140dc153e9e0337db5aad07d4269e6
qais25 <qaisabdullahbafayadh@gmail.com>
2024-10-27 16:02:59 +0300

    Update store_offer_product.dart

--- lib/store_app_features/offer_product/ui/screen/store_offer_product.dart
+++ lib/store_app_features/offer_product/ui/screen/store_offer_product.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sindbad_management_app/core/widgets/custom_appbar_widget.dart';
-import 'package:sindbad_management_app/store_app_features/offer_product/ui/widget/store_offer_product_widget.dart';
+import 'package:sindbad_management_app/store_app_features/offer_product/ui/widget/store_offer_product_body.dart';
 
 class StoreOfferProduct extends StatelessWidget {
   const StoreOfferProduct({super.key});
@@ -11,10 +11,7 @@ class StoreOfferProduct extends StatelessWidget {
       body: Column(
         children: [
           KCustomAppBarWidget(nameAppbar: "المنتجات التي بها عروض"),
-          StoreOfferProductWidget(
-            productNum: '',
-            productName: '',
-          ),
+          StoreOfferProductBody(),
         ],
       ),
     );
