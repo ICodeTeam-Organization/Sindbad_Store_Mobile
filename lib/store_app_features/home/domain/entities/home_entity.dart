792ee5ff495ab8644529d1bd7d602cdef254bf42
qais25 <qaisabdullahbafayadh@gmail.com>
2024-10-27 16:27:31 +0300

    create : home_entity

---
+++ lib/store_app_features/home/domain/entities/home_entity.dart
@@ -0,0 +1,10 @@
+class HomeEntity {
+  final String orderNum;
+  final DateTime? orderDate;
+  final int productNum;
+
+  HomeEntity(
+      {required this.orderNum,
+      required this.orderDate,
+      required this.productNum});
+}
