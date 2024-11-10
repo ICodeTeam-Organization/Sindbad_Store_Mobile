97c80533cbc46c6b15ee1affa17ad2d3eef31c81
qais25 <qaisabdullahbafayadh@gmail.com>
2024-11-10 16:25:24 +0300

    Update text_style.dart

--- lib/core/styles/text_style.dart
+++ lib/core/styles/text_style.dart
@@ -1,39 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-abstract class KTextStyle
-{
-  static TextStyle primaryTitle = TextStyle(
-    fontSize: 20.sp,
-    fontWeight: FontWeight.bold
-  );
+abstract class KTextStyle {
+  static TextStyle textStyle20 =
+      TextStyle(fontSize: 20.sp, fontWeight: FontWeight.w500);
+  static TextStyle textStyle18 =
+      TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w500);
+  static TextStyle textStyle16 =
+      TextStyle(fontSize: 16.sp, fontWeight: FontWeight.bold);
+  static TextStyle textStyle14 =
+      TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w500);
+  static TextStyle textStyle12 =
+      TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w700);
+  //////////////////////////////////////////////
+  ///Old
+  static TextStyle primaryTitle =
+      TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold);
   static TextStyle tabs = TextStyle(
     fontSize: 16.sp,
     fontWeight: FontWeight.bold,
   );
-  static TextStyle secondaryTitle = TextStyle(
-    fontSize: 14.sp,
-    fontWeight: FontWeight.bold
-  );
+  static TextStyle secondaryTitle =
+      TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w500);
   static TextStyle buttonScreen = TextStyle(
     fontSize: 13.sp,
     fontWeight: FontWeight.bold,
   );
-   static TextStyle textStyle12 = TextStyle(
-    fontSize: 12.sp,
-    fontWeight: FontWeight.bold,
-  );
-  /*static const textInput = TextStyle(
-    fontSize: 12,
-    fontWeight: FontWeight.bold,
-  );
-  static const cardInfo = TextStyle(
-    fontSize: 12,
-    fontWeight: FontWeight.bold,
-  );
-  static const buttonCard = TextStyle(
-    fontSize: 12,
-    fontWeight: FontWeight.bold,
-  );*/
-
 }
