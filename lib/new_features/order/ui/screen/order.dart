209147cca0c5d34932b71f9e2ec13192805c44d8
qais25 <qaisabdullahbafayadh@gmail.com>
2024-11-16 10:14:06 +0300

    Create order.dart

---
+++ lib/new_features/order/ui/screen/order.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Order extends StatelessWidget {
+  const Order({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        width: 200,
+        height: 200,
+        color: Colors.red,
+      ),
+    );
+  }
+}
