811f2bfbdc554759a44d1966c9ac31e2022ccf42
qais25 <qaisabdullahbafayadh@gmail.com>
2024-11-16 10:13:11 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sindbad_management_app/core/utils/route.dart';
+import 'package:sindbad_management_app/new_features/auth/data/repos_impl/sign_in_repo_impl.dart';
+import 'package:sindbad_management_app/new_features/auth/domain/usecases/sign_in_usecase.dart';
+
+import 'core/setup_service_locator.dart';
+import 'core/simple_bloc_observer.dart';
+import 'new_features/auth/ui/manager/cubit/sign_in_cubit.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  setupServiceLocator();
+  Bloc.observer = SimpleBlocObserver();
   runApp(const SindbadManagementApp());
 }
 
@@ -13,32 +23,38 @@ class SindbadManagementApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(375, 650),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      builder: (context, child) => MaterialApp.router(
-        routerConfig: AppRouter.router,
-        theme: ThemeData(
-          textTheme: GoogleFonts.almaraiTextTheme(
-            Theme.of(context).textTheme,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: (context) => SignInCubit(SignInUseCase(
+                  getit.get<SignInRepoImpl>(),
+                ))),
+      ],
+      child: ScreenUtilInit(
+        designSize: const Size(375, 650),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) => MaterialApp.router(
+          routerConfig: AppRouter.router,
+          theme: ThemeData(
+            textTheme: GoogleFonts.almaraiTextTheme(
+              Theme.of(context).textTheme,
+            ),
+
+            scaffoldBackgroundColor:
+                const Color(0xFFF9F9F9), // Set default background color
           ),
-
-          scaffoldBackgroundColor:
-              const Color(0xFFF9F9F9), // Set default background color
+          localizationsDelegates: const [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('ar', 'AR'), // Arabic, no country code
+          ],
+          locale: const Locale('ar', 'AR'),
+          debugShowCheckedModeBanner: false,
         ),
-
-        localizationsDelegates: const [
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        supportedLocales: const [
-          Locale('ar', 'AR'), // Arabic, no country code
-        ],
-        locale: const Locale('ar', 'AR'),
-        debugShowCheckedModeBanner: false,
-
       ),
     );
   }
