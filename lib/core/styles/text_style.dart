c56dde0f962b34d8a773faa1933f7c2b5bee78f9
omerabdooo <omerabdoalamoudi@gmail.com>
2024-11-17 20:09:25 +0300

    re order the textStyle deponding on the fontsize

--- lib/core/styles/text_style.dart
+++ lib/core/styles/text_style.dart
@@ -2,46 +2,64 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 abstract class KTextStyle {
+  // Text styles with font size 20
   static TextStyle textStyle20 =
       TextStyle(fontSize: 20.sp, fontWeight: FontWeight.w500);
+
+  // Text styles with font size 18
   static TextStyle textStyle18 =
       TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w500);
-  static TextStyle textStyle16 =
-      TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w500);
-  static TextStyle textStyle14 =
-      TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w700);
-  static TextStyle textStyle12 =
-      TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w700);
-
   static TextStyle textStyle18w500 =
       TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w500);
+
+  // Text styles with font size 16
+  static TextStyle textStyle16 =
+      TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w500);
+
+  // Text styles with font size 14
+  static TextStyle textStyle14 =
+      TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w700);
   static TextStyle textStyle14w700 =
       TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w700);
   static TextStyle textStyle14w600 =
       TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w600);
   static TextStyle textStyle14w500 =
       TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w500);
+
+  // Text styles with font size 13
   static TextStyle textStyle13w700 =
       TextStyle(fontSize: 13.sp, fontWeight: FontWeight.w700);
   static TextStyle textStyle13w600 =
       TextStyle(fontSize: 13.sp, fontWeight: FontWeight.w600);
   static TextStyle textStyle13w500 =
       TextStyle(fontSize: 13.sp, fontWeight: FontWeight.w500);
+
+  // Text styles with font size 12
+  static TextStyle textStyle12 =
+      TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w700);
   static TextStyle textStyle12w700 =
-      TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w700);//
+      TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w700);
   static TextStyle textStyle12w600 =
-      TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w600);//
+      TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w600);
   static TextStyle textStyle12w500 =
-      TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w500);//
+      TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w500);
+
+  // Text styles with font size 11
   static TextStyle textStyle11w700 =
-      TextStyle(fontSize: 11.sp, fontWeight: FontWeight.w700);//
+      TextStyle(fontSize: 11.sp, fontWeight: FontWeight.w700);
+
+  // Text styles with font size 10
   static TextStyle textStyle10w500 =
-      TextStyle(fontSize: 10.sp, fontWeight: FontWeight.w500);//
+      TextStyle(fontSize: 10.sp, fontWeight: FontWeight.w500);
+
+  // Text styles with font size 9
   static TextStyle textStyle9w700 =
-      TextStyle(fontSize: 9.sp, fontWeight: FontWeight.w700);//  //////////////////////////////////////////////
+      TextStyle(fontSize: 9.sp, fontWeight: FontWeight.w700);
   static TextStyle textStyle9w600 =
-      TextStyle(fontSize: 9.sp, fontWeight: FontWeight.w600);//  //////////////////////////////////////////////
-  ///Old
+      TextStyle(fontSize: 9.sp, fontWeight: FontWeight.w600);
+
+///////// -- Old -- //////////
+
   static TextStyle primaryTitle =
       TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold);
   static TextStyle tabs = TextStyle(
