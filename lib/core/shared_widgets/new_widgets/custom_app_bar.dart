ccbe50b428df49f0734934627e7cec96ca2c3ea2
Bot200037 <salem.baqatyan775@gmail.com>
2024-11-18 09:59:56 +0300

    Update custom_app_bar.dart

--- lib/core/shared_widgets/new_widgets/custom_app_bar.dart
+++ lib/core/shared_widgets/new_widgets/custom_app_bar.dart
@@ -1,39 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:sindbad_management_app/core/styles/text_style.dart';
 
 import '../../styles/Colors.dart';
 
 class CustomAppBar extends StatelessWidget {
-  CustomAppBar({
+  final String tital;
+  final VoidCallback? onPressed;
+  const CustomAppBar({
     super.key,
     required this.tital,
     this.onPressed,
   });
-  String tital;
-  VoidCallback? onPressed;
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      backgroundColor: AppColors.primaryBackground,
+      backgroundColor: AppColors.white,
+      toolbarHeight: 50.h,
       centerTitle: true,
       title: Text(
-        tital,
+        tital, 
+        style: KTextStyle.textStyle18.copyWith(color: AppColors.blackLight,),
       ),
       actions: [
         Padding(
           padding: const EdgeInsets.only(left: 8),
-          child: Container(
-            height: 40.h,
-            width: 40.w,
-            decoration: BoxDecoration(
-                border: Border.all(color: AppColors.grey, width: 2),
-                borderRadius: BorderRadius.circular(16),
-                color: AppColors.primaryBackground),
-            child: IconButton(
-              onPressed: onPressed,
-              iconSize: 24,
-              icon: Icon(Icons.search),
-            ),
+          child: IconButton(
+            onPressed: onPressed,
+            iconSize: 24,
+            icon: SvgPicture.asset(
+                      "assets/search_appbar.svg",
+                      width: 40.w,
+                      height: 40.w,
+                    ),
           ),
         ),
       ],
