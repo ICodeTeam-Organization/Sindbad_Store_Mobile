1405730840a3e63e9995dd22840d85da18a0d728
qais25 <qaisabdullahbafayadh@gmail.com>
2024-11-18 22:40:53 +0300

    Update order_management_screen.dart

--- lib/features/order_management _features/ui/screen/order_management_screen.dart
+++ lib/features/order_management _features/ui/screen/order_management_screen.dart
@@ -20,7 +20,6 @@ class OrderManagementScreen extends StatelessWidget {
           child: Column(
             children: [
               CustomAppBar(
-                onPressed: () {},
                 tital: 'قائمة الطلبات',
               ),
               SizedBox(height: 5),
@@ -29,25 +28,27 @@ class OrderManagementScreen extends StatelessWidget {
                   length: 4,
                   tabs: [
                     Tab(
-                      child: Text('الجديدة'),
+                      child: Text('الجديدة', style: KTextStyle.textStyle16),
                     ),
                     Tab(
-                      child: Text('المستعجلة'),
+                      child: Text('المستعجلة', style: KTextStyle.textStyle16),
                     ),
                     Tab(
-                      child: Text('السابقة'),
+                      child: Text('السابقة', style: KTextStyle.textStyle16),
                     ),
                     Tab(
-                      child: Text('الملغية'),
+                      child: Text('الملغية', style: KTextStyle.textStyle16),
                     ),
                   ],
                   tabViews: [
+                    //New tabViews
                     SubCustomTabBar(
                       length: 5,
                       tabs: [
                         Tab(
-                          child:
-                              Text('الكل', style: KTextStyle.textStyle12w700),
+                          child: Text('الكل',
+                              style: KTextStyle.textStyle14
+                                  .copyWith(fontWeight: FontWeight.w500)),
                         ),
                         Tab(
                           child: Stack(
@@ -56,150 +57,32 @@ class OrderManagementScreen extends StatelessWidget {
                               Positioned(
                                 width: 110,
                                 child: Text('بدون فاتورة',
-                                    style: KTextStyle.textStyle11w700
-                                        .copyWith(fontWeight: FontWeight.bold)),
+                                    style: KTextStyle.textStyle11
+                                        .copyWith(fontWeight: FontWeight.w500)),
                               ),
                             ],
                           ),
                         ),
                         Tab(
-                          child: Text('لم تسدد'),
+                          child: Text(
+                            'لم تسدد',
+                            style: KTextStyle.textStyle14
+                                .copyWith(fontWeight: FontWeight.w500),
+                          ),
                         ),
                         Tab(
-                          child: Text('للتجهيز'),
+                          child: Text('للتجهيز',
+                              style: KTextStyle.textStyle14
+                                  .copyWith(fontWeight: FontWeight.w500)),
                         ),
                         Tab(
-                          child: Text('للشحن'),
+                          child: Text('للشحن',
+                              style: KTextStyle.textStyle14
+                                  .copyWith(fontWeight: FontWeight.w500)),
                         ),
                       ],
                       tabViews: [
-                        ListView.builder(
-                          itemCount: 10,
-                          itemBuilder: (BuildContext context, int index) {
-                            return InkWell(
-                              onTap: () {
-                                context.push(
-                                  AppRouter.storeRouters.details,
-                                  // extra: idOrder,
-                                );
-                              },
-                              child: Container(
-                                margin: EdgeInsets.symmetric(vertical: 10.h),
-                                decoration: BoxDecoration(
-                                    border: Border.all(color: AppColors.grey),
-                                    borderRadius: BorderRadius.circular(16.r)),
-                                height: 160.h,
-                                width: 380.w,
-                                child: Column(
-                                  children: [
-                                    Container(
-                                      height: 60.h,
-                                      decoration: BoxDecoration(
-                                          color: AppColors.grey,
-                                          borderRadius:
-                                              BorderRadius.circular(16.r)),
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceAround,
-                                        children: [
-                                          Column(
-                                            children: [
-                                              Text(
-                                                'رقم الطلب',
-                                                style:
-                                                    KTextStyle.textStyle11w700,
-                                              ),
-                                              Text(
-                                                '1111111111',
-                                                style:
-                                                    KTextStyle.textStyle14w700,
-                                              ),
-                                            ],
-                                          ),
-                                          Column(
-                                            children: [
-                                              Text('رقم الفاتورة',
-                                                  style: KTextStyle
-                                                      .textStyle11w700),
-                                              Text(
-                                                '454645454',
-                                                style:
-                                                    KTextStyle.textStyle14w700,
-                                              ),
-                                            ],
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 10.w,
-                                    ),
-                                    Container(
-                                      padding: EdgeInsets.symmetric(
-                                          horizontal: 16.w),
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.start,
-                                        children: [
-                                          Icon(Icons.alarm),
-                                          Text(
-                                            '4:15 - التاريخ 2024/11/17 ',
-                                            style: KTextStyle.textStyle12w700,
-                                          ),
-                                          SizedBox(
-                                            width: 80.w,
-                                          ),
-                                          SizedBox(
-                                            width: 40.w,
-                                            height: 40.h,
-                                            child: Stack(children: [
-                                              Align(
-                                                alignment:
-                                                    Alignment.bottomCenter,
-                                                child: SvgPicture.asset(
-                                                  "assets/Bag.svg",
-                                                  width: 40.w,
-                                                  height: 40.h,
-                                                ),
-                                              ),
-                                              Align(
-                                                alignment:
-                                                    Alignment.bottomCenter,
-                                                child: Text(
-                                                  "25",
-                                                  style: KTextStyle.textStyle12
-                                                      .copyWith(
-                                                    color: AppColors.white,
-                                                  ),
-                                                ),
-                                              ),
-                                            ]),
-                                          ),
-                                          Text('عدد الاصناف',
-                                              style:
-                                                  KTextStyle.textStyle12w600),
-                                        ],
-                                      ),
-                                    ),
-                                    Container(
-                                      padding: EdgeInsets.symmetric(
-                                          horizontal: 20.w),
-                                      child: Row(
-                                        children: [
-                                          Text('بيانات السداد : ',
-                                              style:
-                                                  KTextStyle.textStyle12w700),
-                                          Text('لا يوجد',
-                                              style: KTextStyle.textStyle12w600)
-                                        ],
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            );
-                          },
-                        ),
+                        InfoOrder(),
                         Text('data'),
                         Text('data'),
                         Text('data'),
@@ -255,6 +138,132 @@ class OrderManagementScreen extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class InfoOrder extends StatelessWidget {
+  const InfoOrder({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (BuildContext context, int index) {
+        return InkWell(
+          onTap: () {
+            context.push(
+              AppRouter.storeRouters.details,
+              // extra: idOrder,
+            );
+          },
+          child: Container(
+            margin: EdgeInsets.symmetric(vertical: 10.h),
+            decoration: BoxDecoration(
+                border: Border.all(color: AppColors.grey),
+                borderRadius: BorderRadius.circular(16.r)),
+            height: 160.h,
+            width: 380.w,
+            child: Column(
+              children: [
+                Container(
+                  height: 60.h,
+                  decoration: BoxDecoration(
+                      color: AppColors.grey,
+                      borderRadius: BorderRadius.circular(16.r)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            'رقم الطلب',
+                            style: KTextStyle.textStyle11,
+                          ),
+                          Text(
+                            '1111111111',
+                            style: KTextStyle.textStyle14,
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text('رقم الفاتورة', style: KTextStyle.textStyle11),
+                          Text(
+                            '454645454',
+                            style: KTextStyle.textStyle14,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 10.h,
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 16.w),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SvgPicture.asset(
+                        "assets/alarm.svg",
+                        width: 30.w,
+                        height: 30.h,
+                      ),
+                      SizedBox(
+                        width: 10.w,
+                      ),
+                      Text(
+                        '4:15 - التاريخ 2024/11/17 ',
+                        style: KTextStyle.textStyle12,
+                      ),
+                      SizedBox(
+                        width: 70.w,
+                      ),
+                      SizedBox(
+                        width: 40.w,
+                        height: 40.h,
+                        child: Stack(children: [
+                          Align(
+                            alignment: Alignment.bottomCenter,
+                            child: SvgPicture.asset(
+                              "assets/Bag.svg",
+                              width: 40.w,
+                              height: 40.h,
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.bottomCenter,
+                            child: Text(
+                              "25",
+                              style: KTextStyle.textStyle12.copyWith(
+                                color: AppColors.white,
+                              ),
+                            ),
+                          ),
+                        ]),
+                      ),
+                      Text('عدد الاصناف', style: KTextStyle.textStyle12),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20.w),
+                  child: Row(
+                    children: [
+                      Text('بيانات السداد : ', style: KTextStyle.textStyle12),
+                      Text('لا يوجد', style: KTextStyle.textStyle12)
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
