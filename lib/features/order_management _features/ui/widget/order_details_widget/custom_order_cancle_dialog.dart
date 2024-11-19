b814ea082a583d85c01ef018143e57b12d4ab639
qais25 <qaisabdullahbafayadh@gmail.com>
2024-11-19 22:40:09 +0300

    Update custom_order_cancle_dialog.dart

--- lib/features/order_management _features/ui/widget/order_details_widget/custom_order_cancle_dialog.dart
+++ lib/features/order_management _features/ui/widget/order_details_widget/custom_order_cancle_dialog.dart
@@ -1,29 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:intl/intl.dart';
-
 import '../../../../../core/shared_widgets/new_widgets/store_primary_button.dart';
 import '../../../../../core/styles/Colors.dart';
 import '../../../../../core/styles/text_style.dart';
 import '../../function/image_picker_function.dart';
 
-TextEditingController dateConroller = TextEditingController();
-TextEditingController numberConroller = TextEditingController();
-TextEditingController mountConroller = TextEditingController();
+TextEditingController cancelConroller = TextEditingController();
 
 class CustomOrderCancleDialog extends StatefulWidget {
   final String headTitle;
   final String firstTitle;
-  final String secondTitle;
-  final String thierdTitle;
   final GestureTapCallback onPressedSure;
   const CustomOrderCancleDialog({
     super.key,
     required this.headTitle,
     required this.firstTitle,
-    required this.secondTitle,
-    required this.thierdTitle,
     required this.onPressedSure,
   });
 
@@ -61,9 +53,7 @@ class _CustomOrderCancleDialogState extends State<CustomOrderCancleDialog> {
           IconButton(
             onPressed: () {
               Navigator.of(context).pop();
-              dateConroller.clear();
-              numberConroller.clear();
-              mountConroller.clear();
+              cancelConroller.clear();
             },
             icon: SvgPicture.asset(
               "assets/cancle.svg",
@@ -80,30 +70,14 @@ class _CustomOrderCancleDialogState extends State<CustomOrderCancleDialog> {
     return Container(
       padding: EdgeInsets.symmetric(vertical: 5.h),
       width: MediaQuery.sizeOf(context).width,
-      height: MediaQuery.sizeOf(context).height,
+      height: MediaQuery.sizeOf(context).height * 0.4,
       color: Colors.transparent,
       child: SingleChildScrollView(
         child: Column(
           children: [
             _buildInfoRow(
-              title: widget.firstTitle,
-              info: dateConroller,
-              isDate: true,
-              context: context,
+              info: cancelConroller,
             ),
-            _buildInfoRow(
-              title: widget.secondTitle,
-              info: numberConroller,
-              isDate: false,
-              context: context,
-            ),
-            _buildInfoRow(
-              title: widget.thierdTitle,
-              info: mountConroller,
-              isDate: false,
-              context: context,
-            ),
-            _buildImageSection(context),
             SizedBox(
               height: 7.h,
             ),
@@ -118,58 +92,21 @@ class _CustomOrderCancleDialogState extends State<CustomOrderCancleDialog> {
   }
 
   Widget _buildInfoRow({
-    required String title,
     required TextEditingController info,
-    required bool isDate,
-    required context,
   }) {
     return Padding(
       padding: EdgeInsets.only(bottom: 5.h),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Text(
-            title,
-            style: KTextStyle.textStyle12,
-          ),
           SizedBox(
-            width: 10.w,
-          ),
-          SizedBox(
-            width: 220.w,
-            height: 48.h,
+            width: MediaQuery.sizeOf(context).width * 0.7,
+            height: MediaQuery.sizeOf(context).height * 0.3,
             child: TextField(
-              readOnly: isDate,
-              keyboardType: TextInputType.number,
+              maxLines: 10,
+              keyboardType: TextInputType.text,
               controller: info,
               decoration: InputDecoration(
-                prefixIcon: isDate
-                    ? InkWell(
-                        onTap: () async {
-                          DateTime? pickeddate = await showDatePicker(
-                            context: context,
-                            initialDate: DateTime.now(),
-                            firstDate: DateTime(1980),
-                            lastDate: DateTime(2101),
-                          );
-                          if (pickeddate != null) {
-                            setState(
-                              () {
-                                dateConroller.text =
-                                    DateFormat('yyyy/MM/dd').format(pickeddate);
-                              },
-                            );
-                          }
-                        },
-                        child: Padding(
-                          padding: EdgeInsets.all(5),
-                          child: SizedBox(
-                            width: 10.w,
-                            height: 10.h,
-                            child: SvgPicture.asset('assets/calendar2.svg'),
-                          ),
-                        )) // أيقونة التاريخ
-                    : null,
                 contentPadding:
                     EdgeInsets.symmetric(vertical: 5.h, horizontal: 10.w),
                 border: OutlineInputBorder(
@@ -182,33 +119,6 @@ class _CustomOrderCancleDialogState extends State<CustomOrderCancleDialog> {
               style: KTextStyle.secondaryTitle.copyWith(color: AppColors.black),
             ),
           ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildImageSection(context) {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 10.h),
-      width: MediaQuery.sizeOf(context).width,
-      decoration: BoxDecoration(
-          border: Border.all(color: AppColors.grey),
-          borderRadius: BorderRadius.circular(16.r)),
-      child: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 8.h),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  "صورة الفاتورة",
-                  style: KTextStyle.textStyle14,
-                ),
-              ],
-            ),
-          ),
-          const ImagePickerFunction(),
         ],
       ),
     );
