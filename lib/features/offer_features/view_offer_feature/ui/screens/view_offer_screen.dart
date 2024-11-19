f84763a6a285fd14fda7bfae017b0a2f7e3d25ec
Bot200037 <salem.baqatyan775@gmail.com>
2024-11-19 10:18:47 +0300

    Update view_offer_screen.dart

--- lib/features/offer_features/view_offer_feature/ui/screens/view_offer_screen.dart
+++ lib/features/offer_features/view_offer_feature/ui/screens/view_offer_screen.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:sindbad_management_app/core/shared_widgets/new_widgets/custom_app_bar.dart';
 import 'package:sindbad_management_app/core/styles/Colors.dart';
-import 'package:sindbad_management_app/core/styles/text_style.dart';
 import 'package:sindbad_management_app/core/utils/route.dart';
 import 'package:sindbad_management_app/features/offer_features/view_offer_feature/ui/widgets/action_button_widget.dart';
 import 'package:sindbad_management_app/features/offer_features/view_offer_feature/ui/widgets/card_offer_widget.dart';
@@ -47,18 +44,19 @@ class _ViewOfferScreenState extends State<ViewOfferScreen> {
                         InkWell(
                           onTap: () {
                             context.push(AppRouter.storeRouters.kOfferProductDetails,
-                            extra: ['يوم الجمعة','اشتري x واحصل على y',]
+                            extra: ['يوم الجمعة','discount',]
                             // extra: [offerName,offerType,]
                           );  // Use GoRouter to navigate to the parameterized route
                           },
                           child: CardOfferWidget(
                             offerName: 'يوم الجمعة',
-                            offerType: 'اشتري x واحصل على y', 
-                            // "10% من إجمالي الخصم"
+                            discountRate: '10%',
+                            offerBouns: 'اشتري x واحصل على y',
                             startOffer: DateTime.utc(2024, 5, 1), 
                             endOffer: DateTime.utc(2024, 11, 18), 
-                            countProducts: 17, 
                             isActive: true,
+                            countProducts: "17", 
+                            offerType: 'discount', 
                           ),
                         ),
                         Divider(),
