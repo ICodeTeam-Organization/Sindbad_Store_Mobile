1c255850a1bf63028b4dd1216f775f1108982254
Qais Bafayadh <qaisabdullahbafayadh@gmail.com>
2024-11-27 20:03:46 +0300

    Update drop_down_widget.dart

--- lib/features/order_management _features/ui/widget/order_shipping_widgets/drop_down_widget.dart
+++ lib/features/order_management _features/ui/widget/order_shipping_widgets/drop_down_widget.dart
@@ -1,13 +1,16 @@
 import 'package:animated_custom_dropdown/custom_dropdown.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'dart:developer';
 import 'package:sindbad_management_app/core/styles/Colors.dart';
+import 'package:sindbad_management_app/core/styles/text_style.dart';
 
 const List<String> _list = [
   'الافضل',
   'البراق',
   'السريع',
   'توصيل',
+  'اخرى',
 ];
 // SingleSelectController dropDownController = SingleSelectController(String value);
 
@@ -16,19 +19,32 @@ class DropDownWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: CustomDropdown(
-        // controller: dropDownController ,
-        items: _list,
-        initialItem: _list[0],
-        onChanged: (value) {
-          log('changing value to: $value');
-        },
-        decoration: CustomDropdownDecoration(
-            closedFillColor: Colors.transparent,
-            closedBorder: Border.all(color: AppColors.grey)),
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 5.h,
+        ),
+        Text(
+          'اسم الشركة الناقلة',
+          style: KTextStyle.textStyle12.copyWith(fontWeight: FontWeight.w500),
+        ),
+        CustomDropdown(
+          closedHeaderPadding: EdgeInsets.all(5),
+          // controller: dropDownController ,
+          items: _list,
+          initialItem: _list[0],
+          onChanged: (value) {
+            log('changing value to: $value');
+          },
+          decoration: CustomDropdownDecoration(
+              closedFillColor: Colors.transparent,
+              closedBorder: Border.all(color: AppColors.grey)),
+        ),
+        SizedBox(
+          height: 5.h,
+        ),
+      ],
     );
   }
 }
