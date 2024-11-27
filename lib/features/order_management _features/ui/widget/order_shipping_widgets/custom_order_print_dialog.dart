72bc8eeea7be667bfe6402c75ddd01697ea9056a
Qais Bafayadh <qaisabdullahbafayadh@gmail.com>
2024-11-27 13:49:52 +0300

    Update custom_order_print_dialog.dart

--- lib/features/order_management _features/ui/widget/order_shipping_widgets/custom_order_print_dialog.dart
+++ lib/features/order_management _features/ui/widget/order_shipping_widgets/custom_order_print_dialog.dart
@@ -8,6 +8,7 @@ import '../../../../../core/shared_widgets/new_widgets/store_primary_button.dart
 import '../../../../../core/styles/Colors.dart';
 import '../../../../../core/styles/text_style.dart';
 import '../../function/pdf.dart';
+import 'build_info_row_add.dart';
 
 TextEditingController printConroller = TextEditingController();
 
@@ -56,98 +57,6 @@ class CustomOrderPrintDialog extends StatelessWidget {
           // Share.shareFiles([share], text: 'إليك هذا الملف!');
         },
         parcels: parcels,
-      ),
-    );
-  }
-}
-
-// ignore: must_be_immutable
-class BuildInfoRowAdd extends StatefulWidget {
-  BuildInfoRowAdd({super.key, required this.parcels, required this.title});
-  int? parcels;
-  String? title;
-
-  @override
-  State<BuildInfoRowAdd> createState() => _BuildInfoRowAddState();
-}
-
-class _BuildInfoRowAddState extends State<BuildInfoRowAdd> {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(bottom: 5.h),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Text(
-            widget.title!,
-            style: KTextStyle.textStyle13,
-          ),
-          SizedBox(
-            width: 20.w,
-          ),
-          DecoratedBox(
-            decoration: BoxDecoration(
-                border: Border.all(color: AppColors.grey),
-                borderRadius: BorderRadius.circular(100.r)),
-            child: SizedBox(
-              width: 35.w,
-              height: 32.h,
-              child: IconButton(
-                iconSize: 24,
-                padding: EdgeInsets.zero,
-                onPressed: () {
-                  setState(() {
-                    widget.parcels = (widget.parcels! + 1);
-                    print(widget.parcels);
-                    // count = widget.parcels;
-                  });
-                },
-                icon: Icon(Icons.add),
-              ),
-            ),
-          ),
-          SizedBox(
-            width: 20.w,
-          ),
-          SizedBox(
-            width: 50.w,
-            height: 45.h,
-            child: DecoratedBox(
-                decoration: BoxDecoration(
-                    border: Border.all(color: AppColors.grey),
-                    borderRadius: BorderRadius.circular(12.r)),
-                child: Center(
-                  child: Text(
-                    '${widget.parcels}',
-                    style: KTextStyle.textStyle12,
-                  ),
-                )),
-          ),
-          SizedBox(
-            width: 20.w,
-          ),
-          DecoratedBox(
-            decoration: BoxDecoration(
-                border: Border.all(color: AppColors.grey),
-                borderRadius: BorderRadius.circular(100.r)),
-            child: SizedBox(
-              width: 35.w,
-              height: 32.h,
-              child: IconButton(
-                iconSize: 24,
-                padding: EdgeInsets.zero,
-                onPressed: () {
-                  setState(() {
-                    widget.parcels = (widget.parcels! - 1);
-                    // count = widget.parcels;
-                  });
-                },
-                icon: Icon(Icons.cancel),
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
