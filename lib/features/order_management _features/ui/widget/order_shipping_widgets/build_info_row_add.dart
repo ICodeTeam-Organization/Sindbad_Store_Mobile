1bcd3ed27db430a1c01f74bdef7cfc381074071c
Qais Bafayadh <qaisabdullahbafayadh@gmail.com>
2024-11-27 20:03:39 +0300

    Create build_info_row_add.dart

---
+++ lib/features/order_management _features/ui/widget/order_shipping_widgets/build_info_row_add.dart
@@ -0,0 +1,97 @@
+// ignore: must_be_immutable
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sindbad_management_app/core/styles/text_style.dart';
+
+import '../../../../../core/styles/Colors.dart';
+
+// ignore: must_be_immutable
+class BuildInfoRowAdd extends StatefulWidget {
+  BuildInfoRowAdd({super.key, required this.parcels, required this.title});
+  int? parcels;
+  String? title;
+
+  @override
+  State<BuildInfoRowAdd> createState() => _BuildInfoRowAddState();
+}
+
+class _BuildInfoRowAddState extends State<BuildInfoRowAdd> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 5.h),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            widget.title!,
+            style: KTextStyle.textStyle13,
+          ),
+          SizedBox(
+            width: 20.w,
+          ),
+          DecoratedBox(
+            decoration: BoxDecoration(
+                border: Border.all(color: AppColors.grey),
+                borderRadius: BorderRadius.circular(100.r)),
+            child: SizedBox(
+              width: 35.w,
+              height: 38.h,
+              child: IconButton(
+                iconSize: 20,
+                padding: EdgeInsets.zero,
+                onPressed: () {
+                  setState(() {
+                    widget.parcels = (widget.parcels! + 1);
+                    // count = widget.parcels;
+                  });
+                },
+                icon: Icon(Icons.add),
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 20.w,
+          ),
+          SizedBox(
+            width: 48.w,
+            height: 52.h,
+            child: DecoratedBox(
+                decoration: BoxDecoration(
+                    border: Border.all(color: AppColors.grey),
+                    borderRadius: BorderRadius.circular(12.r)),
+                child: Center(
+                  child: Text(
+                    '${widget.parcels}',
+                    style: KTextStyle.textStyle12,
+                  ),
+                )),
+          ),
+          SizedBox(
+            width: 20.w,
+          ),
+          DecoratedBox(
+            decoration: BoxDecoration(
+                border: Border.all(color: AppColors.grey),
+                borderRadius: BorderRadius.circular(100.r)),
+            child: SizedBox(
+              width: 35.w,
+              height: 38.h,
+              child: IconButton(
+                iconSize: 20,
+                padding: EdgeInsets.zero,
+                onPressed: () {
+                  setState(() {
+                    widget.parcels = (widget.parcels! - 1);
+                    // count = widget.parcels;
+                  });
+                },
+                icon: Icon(Icons.cancel),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
