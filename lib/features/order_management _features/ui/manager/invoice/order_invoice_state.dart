f7362c77dc2cf7208ca78ec4e374ea0a65a6b05b
Qais Bafayadh <qaisabdullahbafayadh@gmail.com>
2024-12-01 11:26:50 +0300

    Create order_invoice_state.dart

---
+++ lib/features/order_management _features/ui/manager/invoice/order_invoice_state.dart
@@ -0,0 +1,20 @@
+part of 'order_invoice_cubit.dart';
+
+@immutable
+sealed class OrderInvoiceState {}
+
+final class OrderInvoiceInitial extends OrderInvoiceState {}
+
+final class OrderInvoiceLoading extends OrderInvoiceState {}
+
+final class OrderInvoiceFailuer extends OrderInvoiceState {
+  final String errMessage;
+
+  OrderInvoiceFailuer({required this.errMessage});
+}
+
+final class OrderInvoiceSuccess extends OrderInvoiceState {
+  final OrderInvoiceEntity serverMessage;
+
+  OrderInvoiceSuccess({required this.serverMessage});
+}
