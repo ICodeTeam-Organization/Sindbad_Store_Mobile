81a92ccee73b4746d909e5ea46cdd61047f8a99b
Qais Bafayadh <qaisabdullahbafayadh@gmail.com>
2024-12-01 11:34:47 +0300

    add : RefreshPageCubit to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,17 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:sindbad_management_app/core/utils/route.dart';
 import 'package:sindbad_management_app/features/auth_features/data/repos_impl/sign_in_repo_impl.dart';
 import 'package:sindbad_management_app/features/auth_features/domain/usecases/sign_in_usecase.dart';
-import 'package:sindbad_management_app/features/order_management%20_features/ui/manager/cubit/refresh_page_cubit.dart';
+import 'package:sindbad_management_app/features/order_management%20_features/data/repos_impl/all_order_repo_impl.dart';
+import 'package:sindbad_management_app/features/order_management%20_features/domain/usecases/order_invoice_usecasse.dart';
+import 'package:sindbad_management_app/features/order_management%20_features/ui/manager/order_details/order_details_cubit.dart';
+import 'package:sindbad_management_app/features/order_management%20_features/ui/manager/refresh/refresh_page_cubit.dart';
+import 'package:sindbad_management_app/features/order_management%20_features/ui/manager/invoice/order_invoice_cubit.dart';
+import 'package:sindbad_management_app/features/order_management%20_features/ui/screen/order_details.dart';
 
 import 'core/setup_service_locator.dart';
 import 'core/simple_bloc_observer.dart';
 import 'features/auth_features/ui/manager/cubit/sign_in_cubit.dart';
+import 'features/order_management _features/domain/usecases/order_details_usecase.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,7 +36,15 @@ class SindbadManagementApp extends StatelessWidget {
             create: (context) => SignInCubit(SignInUseCase(
                   getit.get<SignInRepoImpl>(),
                 ))),
-        BlocProvider(create: (context) => RefreshPageCubit())
+        BlocProvider(create: (context) => RefreshPageCubit()),
+        BlocProvider(
+            create: (context) => OrderInvoiceCubit(OrderInvoiceUsecase(
+                  allOrderRepo: getit.get<AllOrderRepoImpl>(),
+                ))),
+        BlocProvider(
+            create: (context) => OrderDetailsCubit(OrderDetailsUsecase(
+                  getit.get<AllOrderRepoImpl>(),
+                ))),
       ],
       child: ScreenUtilInit(
         designSize: const Size(428, 1000),
