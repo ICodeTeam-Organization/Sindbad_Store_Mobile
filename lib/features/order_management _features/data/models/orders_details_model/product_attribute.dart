74d820d3a5ef29baab15699ecc563af2535fc47e
Qais Bafayadh <qaisabdullahbafayadh@gmail.com>
2024-12-03 10:40:31 +0300

    Create product_attribute.dart

---
+++ lib/features/order_management _features/data/models/orders_details_model/product_attribute.dart
@@ -0,0 +1,18 @@
+// class ProductAttribute {
+//   String? attributeName;
+//   String? attributeValue;
+
+//   ProductAttribute({this.attributeName, this.attributeValue});
+
+//   factory ProductAttribute.fromJson(Map<String, dynamic> json) {
+//     return ProductAttribute(
+//       attributeName: json['attributeName'] as String?,
+//       attributeValue: json['attributeValue'] as String?,
+//     );
+//   }
+
+//   Map<String, dynamic> toJson() => {
+//         'attributeName': attributeName,
+//         'attributeValue': attributeValue,
+//       };
+// }
