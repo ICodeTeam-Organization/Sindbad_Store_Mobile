ca546229ad0536920b5447c32401585ffab67bf0
Qais Bafayadh <qaisabdullahbafayadh@gmail.com>
2024-12-03 10:50:52 +0300

    Update show_print_and_shipping_order.dart

--- lib/features/order_management _features/ui/widget/order_details_widget/show_print_and_shipping_order.dart
+++ lib/features/order_management _features/ui/widget/order_details_widget/show_print_and_shipping_order.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sindbad_management_app/features/order_management%20_features/ui/function/image_picker_function.dart';
+import 'package:sindbad_management_app/features/order_management%20_features/ui/manager/shipping/shipping_cubit.dart';
+import 'package:sindbad_management_app/features/order_management%20_features/ui/widget/order_body.dart';
+import 'package:sindbad_management_app/features/order_management%20_features/ui/widget/order_shipping_widgets/build_info_row_add.dart';
+import 'package:sindbad_management_app/features/order_management%20_features/ui/widget/order_shipping_widgets/drop_down_widget.dart';
 import '../../../../../core/shared_widgets/new_widgets/store_primary_button.dart';
 import '../../manager/refresh/refresh_page_cubit.dart';
+import '../order_shipping_widgets/build_shipping_dialog_content.dart';
 import '../order_shipping_widgets/custom_order_print_dialog.dart';
 import '../order_shipping_widgets/custom_order_shipping_dialog.dart';
+import 'build_dialog_content.dart';
 import 'messages.dart';
 
 class ShowPrintAndShippingOrder extends StatelessWidget {
@@ -14,6 +21,7 @@ class ShowPrintAndShippingOrder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int shippingNumber;
     return BlocBuilder<RefreshPageCubit, RefreshPageState>(
       builder: (context, state) {
         return Padding(
@@ -30,20 +38,66 @@ class ShowPrintAndShippingOrder extends StatelessWidget {
                     context: context,
                     builder: (context) {
                       return CustomOrderShippingDialog(
-                        parcels: 0,
                         headTitle: 'بيانات فاتورة الشحن',
                         firstTitle: 'تاريخ الفاتورة',
                         secondTitle: 'رقم فاتورة الشحن',
                         thierdTitle: 'الشركة الناقلة',
-                        onPressedSure: () {
+                        onPressedSure: () async {
+                          try {
+                            await context
+                                .read<ShippingCubit>()
+                                .fetchOrderShipping(
+                                    orderId: idOrders!,
+                                    invoiceDate: dateConroller.text,
+                                    shippingNumber: shippingNumber =
+                                        int.parse(numberShippingConroller.text),
+                                    shippingCompany: companyName ?? '',
+                                    shippingImages: images!,
+                                    numberParcels: parcels!);
+                            dateConroller.clear();
+                            numberShippingConroller.clear();
+                            // Navigator.of(context).pop();
+                            // showDialog(
+                            //   context: context,
+                            //   builder: (context) {
+                            //     return Messages(
+                            //       isTrue: true,
+                            //       trueMessage: 'لقد تم الشحن بنجاح',
+                            //       falseMessage: 'هناك خطاء في العملية',
+                            //     );
+                            //   },
+                            // );
+                          } catch (e) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text('هنالك خطا ما  $e'),
+                              ),
+                            );
+                          }
                           Navigator.of(context).pop();
                           showDialog(
                             context: context,
                             builder: (context) {
-                              return Messages(
-                                isTrue: true,
-                                trueMessage: 'لقد تم الشحن بنجاح',
-                                falseMessage: 'هناك خطاء في العملية',
+                              return BlocBuilder<ShippingCubit, ShippingState>(
+                                builder: (context, state) {
+                                  if (state is ShippingSuccess) {
+                                    return Messages(
+                                      isTrue: state.serverMessage.isSuccess,
+                                      trueMessage:
+                                          'لقد تمت الأضافة في قائمة التجهيز',
+                                      falseMessage: 'هناك خطاء في العملية',
+                                    );
+                                  } else if (state is ShippingFailure) {
+                                    return Messages(
+                                      isTrue: false,
+                                      trueMessage:
+                                          'لقد تمت الأضافة في قائمة التجهيز',
+                                      falseMessage: 'هناك خطاء في العملية',
+                                    );
+                                  } else {
+                                    return Text("يوجد خطا");
+                                  }
+                                },
                               );
                             },
                           );
