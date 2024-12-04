97d6f7f81c7b9b408871ff4988652933fcc6b07d
Qais Bafayadh <qaisabdullahbafayadh@gmail.com>
2024-12-04 18:27:41 +0300

    Update order_shipping_entity.dart

--- lib/features/order_management _features/domain/entities/order_shipping_entity.dart
+++ lib/features/order_management _features/domain/entities/order_shipping_entity.dart
@@ -1,21 +1,9 @@
-import 'dart:io';
-
 class OrderShippingEntity {
   final bool isSuccess;
   final String serverMessage;
-  // final DateTime invoiceDate;
-  // final String shippingNumbers;
-  // final String shippingCompany;
-  // final File? invoiceImages;
-  // final int numberParcels;
 
   OrderShippingEntity({
     required this.isSuccess,
     required this.serverMessage,
-    // required this.invoiceDate,
-    // required this.shippingNumbers,
-    // required this.shippingCompany,
-    // required this.invoiceImages,
-    // required this.numberParcels
   });
 }
