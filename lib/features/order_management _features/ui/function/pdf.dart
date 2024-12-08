1d826c9c6f9312008e331ef0829c38455dc7aec1
Qais Bafayadh <qaisabdullahbafayadh@gmail.com>
2024-12-08 23:41:21 +0300

    share : file

--- lib/features/order_management _features/ui/function/pdf.dart
+++ lib/features/order_management _features/ui/function/pdf.dart
@@ -1,3 +1,74 @@
+// import 'dart:io';
+// import 'package:barcode_widget/barcode_widget.dart';
+// import 'package:dartz/dartz.dart' as dartz;
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:open_file/open_file.dart';
+// import 'package:path_provider/path_provider.dart';
+// import 'package:pdf/pdf.dart';
+// import 'package:pdf/widgets.dart' as pw;
+// import 'package:pdf/widgets.dart';
+// import 'package:printing/printing.dart';
+
+// String? share;
+// pw.Document? pdfDocument;
+
+// class Pdf {
+//   static Future<File> generateCenteredText(String text, int? parcels) async {
+//     final pdf = pw.Document();
+//     for (int i = 0; i < parcels!; i++) {
+//       pdf.addPage(
+//         pw.Page(
+//           build: (pw.Context context) {
+//             return pw.Center(
+//               child: pw.BarcodeWidget(
+//                 height: 70.h,
+//                 width: 200.w,
+//                 data: text,
+//                 textPadding: 8,
+//                 barcode: Barcode.code128(),
+//               ),
+//             );
+//           },
+//         ),
+//       );
+//     }
+
+// await Printing.layoutPdf(
+//   onLayout: (PdfPageFormat format) async => pdf.save(),
+// );
+
+//     return saveDocument(name: 'icode.pdf', pdf: pdf);
+//   }
+
+//   static Future<File> saveDocument({
+//     required String name,
+//     required Document pdf,
+//   }) async {
+//     final bytes = await pdf.save();
+
+//     final dir = await getApplicationDocumentsDirectory();
+//     final file = File('${dir.path}/$name');
+
+//     await file.writeAsBytes(bytes);
+
+//     return file;
+//   }
+
+//   static Future openFile(File file) async {
+//     final url = file.path;
+//     share = url;
+
+//     await OpenFile.open(url);
+//   }
+// }
+// import 'dart:io';
+
+// import 'package:barcode_widget/barcode_widget.dart';
+// import 'package:dartz/dartz.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:path_provider/path_provider.dart';
+// import 'package:pdf/widgets.dart' as pw;
+// import 'package:pdf/widgets.dart' as dartz;
 import 'dart:io';
 import 'package:barcode_widget/barcode_widget.dart';
 import 'package:dartz/dartz.dart' as dartz;
@@ -12,9 +83,10 @@ import 'package:printing/printing.dart';
 String? share;
 
 class Pdf {
-  static Future<File> generateCenteredText(String text, int? parcels) async {
+  static Future<pw.Document> generateCenteredText(
+      String text, int parcels) async {
     final pdf = pw.Document();
-    for (int i = 0; i < parcels!; i++) {
+    for (int i = 0; i < parcels; i++) {
       pdf.addPage(
         pw.Page(
           build: (pw.Context context) {
@@ -31,32 +103,26 @@ class Pdf {
         ),
       );
     }
-
-    await Printing.layoutPdf(
-      onLayout: (PdfPageFormat format) async => pdf.save(),
-    );
-
-    return saveDocument(name: 'icode.pdf', pdf: pdf);
+    // await Printing.layoutPdf(
+    //   onLayout: (PdfPageFormat format) async => pdf.save(),
+    // );
+    return pdf; // Return the generated PDF document
   }
 
   static Future<File> saveDocument({
     required String name,
-    required Document pdf,
+    required pw.Document pdf,
   }) async {
     final bytes = await pdf.save();
-
     final dir = await getApplicationDocumentsDirectory();
     final file = File('${dir.path}/$name');
-
     await file.writeAsBytes(bytes);
-
     return file;
   }
 
   static Future openFile(File file) async {
     final url = file.path;
     share = url;
-
     await OpenFile.open(url);
   }
 }
