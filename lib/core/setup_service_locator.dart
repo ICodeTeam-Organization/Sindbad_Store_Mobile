f34f2b4634e296e1004700ec52daacb61928e4d7
Qais Bafayadh <qaisabdullahbafayadh@gmail.com>
2024-12-24 11:43:40 +0300

    Update setup_service_locator.dart

--- lib/core/setup_service_locator.dart
+++ lib/core/setup_service_locator.dart
@@ -35,9 +35,9 @@ import 'package:sindbad_management_app/features/offer_management_features/view_o
 
 import '../features/auth_features/data/data_sources/sign_in_remot_data_source.dart';
 import '../features/auth_features/data/repos_impl/sign_in_repo_impl.dart';
-// import '../features/order_management _features/data/repos_impl/all_order_repo_impl.dart';
-// import '../features/order_management _features/data/data_sources/all_order_remot_data_source.dart';
-// import '../features/order_management _features/data/repos_impl/all_order_repo_impl.dart';
+import '../features/order_management _features/data/repos_impl/all_order_repo_impl.dart';
+import '../features/order_management _features/data/data_sources/all_order_remot_data_source.dart';
+import '../features/order_management _features/data/repos_impl/all_order_repo_impl.dart';
 import '../features/product_features/add_and_edit_product_feature/data/data_source/add_product_to_store_remote_data_source.dart';
 import '../features/product_features/view_product_features/data/repos/view_product_store_repo_impl.dart';
 import 'api_service.dart';
@@ -58,12 +58,12 @@ void setupServiceLocator() {
       ),
     ),
   );
-  // getit.registerSingleton<AllOrderRepoImpl>(AllOrderRepoImpl(
-  //   AllOrderRemotDataSourceImpl(
-  //     getit.get<ApiService>(),
-  //     getit.get<FlutterSecureStorage>(),
-  //   ),
-  // ));
+  getit.registerSingleton<AllOrderRepoImpl>(AllOrderRepoImpl(
+    AllOrderRemotDataSourceImpl(
+      getit.get<ApiService>(),
+      getit.get<FlutterSecureStorage>(),
+    ),
+  ));
   getit.registerSingleton<ViewProductStoreRepoImpl>(
     ViewProductStoreRepoImpl(
         viewProductRemoteDataSource: ViewProductRemoteDataSourceImpl(
