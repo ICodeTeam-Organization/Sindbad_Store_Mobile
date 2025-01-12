42bd4c41effeec5f69adbe7cfa572b92261b5e43
Abdullah Bagar <abdullah.saeed.bagar@gmail.com>
2025-01-12 13:23:54 +0300

    install Device Preview package

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -59,7 +61,13 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
   setupServiceLocator();
   Bloc.observer = SimpleBlocObserver();
-  runApp(const SindbadManagementApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => SindbadManagementApp(), // Wrap your app
+    ),
+    // const SindbadManagementApp()
+  );
 }
 
 class SindbadManagementApp extends StatelessWidget {
@@ -180,6 +188,10 @@ class SindbadManagementApp extends StatelessWidget {
         minTextAdapt: true,
         splitScreenMode: true,
         builder: (context, child) => MaterialApp.router(
+          // for using with the device preview //
+          // locale: DevicePreview.locale(context),
+          builder: DevicePreview.appBuilder,
+          ///////////////////////////////////////
           routerConfig: AppRouter.router,
           theme: ThemeData(
             textTheme: GoogleFonts.cairoTextTheme(
