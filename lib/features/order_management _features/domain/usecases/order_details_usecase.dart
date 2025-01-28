3d52dcc2fcf0c8133611dc75ce8f032f9a8a5a7f
Qais Bafayadh <qaisabdullahbafayadh@gmail.com>
2025-01-28 11:31:05 +0300

    refactor : delete variables unused in order details

--- lib/features/order_management _features/domain/usecases/order_details_usecase.dart
+++ lib/features/order_management _features/domain/usecases/order_details_usecase.dart
@@ -1,6 +1,5 @@
 import 'package:dartz/dartz.dart';
 import 'package:sindbad_management_app/features/order_management%20_features/domain/repos/all_order_repo.dart';
-
 import '../../../../core/errors/failure.dart';
 import '../../../../core/use_cases/param_use_case.dart';
 import '../entities/order_detalis_entity.dart';
@@ -14,37 +13,12 @@ class OrderDetailsUsecase
   @override
   Future<Either<Failure, List<OrderDetailsEntity>>> execute(
       OrderDetailsParam params) async {
-    return await allOrderRepo.fetchOrderDetails(
-      packageId: params.packageId,
-      // orderNumber: params.orderNumber,
-      // billNumber: params.billNumber,
-      // clock: params.clock,
-      // date: params.date,
-      // itemNumber: params.itemNumber,
-      // paymentInfo: params.paymentInfo,
-      // orderStatus: params.orderStatus,
-    );
+    return await allOrderRepo.fetchOrderDetails(packageId: params.packageId);
   }
 }
 
 class OrderDetailsParam {
   final int packageId;
-  // final String orderNumber;
-  // final String billNumber;
-  // final String clock;
-  // final String date;
-  // final String itemNumber;
-  // final String paymentInfo;
-  // final String orderStatus;
 
-  OrderDetailsParam({
-    required this.packageId,
-    // required this.orderNumber,
-    // required this.billNumber,
-    // required this.clock,
-    // required this.date,
-    // required this.itemNumber,
-    // required this.paymentInfo,
-    // required this.orderStatus
-  });
+  OrderDetailsParam({required this.packageId});
 }
